3c3d53f1d487e9df2857b5d5c9fd4f11b98df881
Karthik P <padiyil2004@gmail.com>
2025-10-26 13:58:52 +0530

    Added basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {Routes ,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import NoPage from './Pages/NoPage'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='*' element={<NoPage />} />
+      </Routes> 
+    </>
   )
 }
 
